40cae4094e7cf0592498685ce1d5d3dce78a080f
RoFishy <154754611+RoFishy@users.noreply.github.com>
2025-03-15 10:29:48 -0400

    Added progress bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,33 @@ function App() {
   const [url, setUrl] = useState<string>();
   const [downloadStatus, setDownloadStatus] = useState("Download");
   const [textEnabled, setTextEnabled] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setUrl(event.target.value);
   }
 
+  async function updateProgress() {
+    for(let i = 0; i<= 99; i++) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      setProgress(i);
+    }
+  }
+
   const handleDownloadClick = () => {
     setDownloadStatus("Downloading...");
+    updateProgress();
     setTextEnabled(true);
     if (!url) {
       alert("No URL provided");
+      setTextEnabled(false);
+      setDownloadStatus("Download");
       return;
     }
     fetch(`http://localhost:5000/convert?url=${url}`)
       .then((response) => response.blob())
       .then((blob) => {
+        setProgress(100)
         setDownloadStatus("Downloaded!");
         const url = window.URL.createObjectURL(blob);
         const a = document.createElement('a');
@@ -28,6 +40,7 @@ function App() {
         a.click();
         window.URL.revokeObjectURL(url);
         setTextEnabled(false);
+        setProgress(0)
       })
       .catch((error) => {
         console.error(error); 
@@ -45,6 +58,7 @@ function App() {
       </header>
       <textarea id="input" placeholder="https://youtube.com/" value={url} onChange={handleInputChange} disabled={textEnabled}></textarea>
       <button id="download" onClick={handleDownloadClick}>{downloadStatus}</button>
+      <progress id="progress" value={progress} max="100" style={{display: (textEnabled) ? "block" : "none"}}></progress>
     </div>
   );
 }
